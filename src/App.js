f6c24d6b18ddb04196ecedf656c691cec5ec9c69
TomaszUrbankiewicz <tomasz.urbankiewicz@gmail.com>
2021-05-31 14:39:33 +0200

    initial commit start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./scss/main.scss";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+//import './App.scss';
+
+const Home=()=> {
+  return (
+    <div className="AppTest">
+     <h1>TEST COMPONENT Home</h1>
+    </div>
+  );
+}
+
+export default Home;
