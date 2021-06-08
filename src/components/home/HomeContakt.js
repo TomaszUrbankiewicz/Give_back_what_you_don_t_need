a4cc89b5117f094a8dd543e3d0dc6b93f2828f50
TomaszUrbankiewicz <tomasz.urbankiewicz@gmail.com>
2021-06-09 01:47:40 +0200

    form_validation

--- src/components/home/HomeContakt.js
+++ src/components/home/HomeContakt.js
@@ -3,15 +3,77 @@ import LetteringWithDecoration from "../not_assigned/LetteringWithDecoration";
 
 const HomeContakt = () => {
     const[inputvalue, setInputvalu]=useState({ inputName:"", inputEmail:"", inputMessange:"" });
+    const[error_input, setError_input]=useState({name:true, email:true, messange:true});
+    const[correct_sending, setCorrect_sending]=useState(false)
 
     const change_value = (e) => {
         setInputvalu(pre => {return ({ ...pre,[e.target.name]:e.target.value})}); 
     };
 
+    const validateEmail = (email) => {
+        const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+
     const send = () => {
-        if(inputvalue.inputName==""||inputvalue.inputEmail==""||inputvalue.inputMessange==""){
-            console.log("inputy są puste") 
+        let error=false
+
+        if(inputvalue.inputName=="" || inputvalue.inputName.includes(" ")){
+            setError_input(prew =>{
+                return( {...prew, name:false} );
+            })
+            error=true
+        }
+        else{
+            setError_input(prew =>{
+                return( {...prew, name:true} );
+            })
         };
+
+        if(inputvalue.inputEmail=="" || !validateEmail(inputvalue.inputEmail)){
+            setError_input(prew =>{
+                return( {...prew, email:false} );
+            })
+            error=true
+        }
+        else{
+            setError_input(prew =>{
+                return( {...prew, email:true} );
+            })
+        };
+
+        if(inputvalue.inputMessange=="" || inputvalue.inputMessange.length<120){
+            setError_input(prew =>{
+                return( {...prew, messange:false} );
+            })
+            error=true
+        }
+        else{
+            setError_input(prew =>{
+                return( {...prew, messange:true} );
+            })
+        };
+
+        if(!error){
+            fetch("https://fer-api.coderslab.pl/v1/portfolio/contact",{
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: "POST",
+                body: JSON.stringify({name:inputvalue.inputName, email:inputvalue.inputEmail, message:inputvalue.inputMessange})
+            })
+            .then( response => response.json() )
+            .then( resp => {
+                console.info(resp)
+                if(resp.status=="success"){
+                    setCorrect_sending(true)
+                }
+            })
+            .catch( resp => {
+                console.error(resp)
+            })     
+        }
     };
 
     return(
@@ -23,16 +85,16 @@ const HomeContakt = () => {
                 />
                 <div className="container_form">
                     <div className="form_name_email">
-                        <div className="form_box_input">
+                        <div className={(error_input.name) ? "form_box_input" : "form_box_input form_box_input_name"}>
                             <label>Wisz swoje imię</label>
                             <input type="text" value={inputvalue.inputName} onChange={change_value} name="inputName"></input>
                         </div>
-                        <div className="form_box_input">
+                        <div className={(error_input.email) ? "form_box_input" : "form_box_input form_box_input_email"}>
                             <label>Wisz swój email</label>
                             <input type="email" value={inputvalue.inputEmail} onChange={change_value} name="inputEmail"></input>
                         </div>
                     </div>
-                    <div className="form_messange">
+                    <div className={(error_input.messange) ? "form_box_messange" : "form_box_messange form_messange"}>
                         <label>Wpisz swoją wiadomość</label>
                         <textarea rows="5" value={inputvalue.inputMessange} onChange={change_value} name="inputMessange"></textarea>
                     </div>
